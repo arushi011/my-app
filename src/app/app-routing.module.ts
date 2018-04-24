08c285b88debef5530f24209b332a6207bd70e71
Arushi Bajpai <arushi000002@gmail.com>
2018-04-24 15:58:02 +0530

    preloadingStrategy added for AOT build for lazy loaded feature module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, Component } from '@angular/core';
-import { Routes, RouterModule} from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules} from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { HomeComponent } from './core/home/home.component';
@@ -21,7 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports : [RouterModule.forRoot(routes)],
+    imports : [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
     exports: [RouterModule]
 
 })
